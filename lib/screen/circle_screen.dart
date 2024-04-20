9a84712fbb86eb76069f299121bb2c976e00dfdf
Adarsh Rawal <adarshrawal321@gmail.com>
2024-04-20 23:13:07 +0545

    Edited the circle screen validators and designs

--- lib/screen/circle_screen.dart
+++ lib/screen/circle_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_application/model/circle_model.dart';
 
 class AreaO extends StatefulWidget {
-  const AreaO({Key? key}) : super(key: key);
+  const AreaO({super.key});
 
   @override
   State<AreaO> createState() => _AreaOState();
@@ -17,37 +18,56 @@ class _AreaOState extends State<AreaO> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        key: myKey,
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Enter  Amount',
+      appBar: AppBar(
+        title: const Text("Area of Circle"),
+        backgroundColor: Colors.lightGreen,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.yellow,
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Form(
+          key: myKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter  Amount',
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a principle amount';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    radius = double.tryParse(value);
+                  });
+                },
               ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a principle amount';
-                }
-                return null;
-              },
-              onChanged: (value) {
-                setState(() {
-                  radius = double.tryParse(value);
-                });
-              },
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  circleModel = CircleModel(radius: radius!);
-                  area = circleModel?.calcCircle();
-                });
-              },
-              child: const Text("Calculate"),
-            ),
-            Text("Area of Circle is $area")
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  if (myKey.currentState!.validate()) {
+                    setState(() {
+                      circleModel = CircleModel(radius: radius!);
+                      area = circleModel?.calcCircle();
+                    });
+                  }
+                },
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                child: const Text("Calculate"),
+              ),
+              Center(
+                  child: Text(
+                "Area of Circle is $area",
+                style: const TextStyle(fontSize: 16.0),
+              ))
+            ],
+          ),
         ),
       ),
     );

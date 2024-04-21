3bf93090d81519cd25818f6b0ea0e54f33730d4d
Adarsh Rawal <adarshrawal321@gmail.com>
2024-04-21 10:38:51 +0545

    Added More navigation on Dashboard

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/screen/circle_screen.dart';
+import 'package:flutter_application/screen/dashboard_screen.dart';
 import 'package:flutter_application/screen/rectangle_screen.dart';
 
 class App extends StatelessWidget {
@@ -9,7 +10,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RectangleArea(),
+      home: Dashboard(),
     );
   }
 }
--- lib/screen/dashboard_screen.dart
+++ lib/screen/dashboard_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/screen/add_two_nums_screen.dart';
+import 'package:flutter_application/screen/circle_screen.dart';
+import 'package:flutter_application/screen/rectangle_screen.dart';
 import 'package:flutter_application/screen/simple_intrest_screen.dart';
 
 class Dashboard extends StatelessWidget {
@@ -8,7 +10,7 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.amber[100],
+        backgroundColor: Colors.green[100],
         appBar: AppBar(
           backgroundColor: Colors.amber[100],
           title: const Text("Dashboard"),
@@ -38,6 +40,22 @@ class Dashboard extends StatelessWidget {
                   },
                   child: const Text("Simple Interest"),
                 ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const AreaO()));
+                    },
+                    child: const Text("Area of circle  Calculator")),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const RectangleArea()));
+                    },
+                    child: const Text("Area of Rectangle Calculator"))
               ],
             )));
   }

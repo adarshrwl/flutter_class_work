a873833c90d2ca0cd4d5478e31344ceb1cbbf0f1
Adarsh Rawal <adarshrawal321@gmail.com>
2024-04-24 14:04:24 +0545

    Added Radio Button

--- lib/screen/mathematical_screen.dart
+++ lib/screen/mathematical_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Mathematical extends StatefulWidget {
   const Mathematical({super.key});
@@ -9,6 +12,7 @@ class Mathematical extends StatefulWidget {
 }
 
 class _MathematicalState extends State<Mathematical> {
+  int _value = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,11 +20,87 @@ class _MathematicalState extends State<Mathematical> {
         title: const Text("Adarsh Rawal"),
         centerTitle: true,
         backgroundColor: Colors.blue,
+        elevation: 0,
       ),
       body: Column(
         children: [
-          TextFormField(),
-          TextFormField(),
+          const SizedBox(
+            height: 8.0,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red))),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red))),
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
+          Row(
+            children: [
+              Radio(
+                  value: 1,
+                  groupValue: _value,
+                  onChanged: (value) {
+                    setState(() {
+                      _value = value!;
+                    });
+                  }),
+              const SizedBox(height: 8),
+              const Text("Addition")
+            ],
+          ),
+          Row(
+            children: [
+              Radio(
+                  value: 2,
+                  groupValue: _value,
+                  onChanged: (value) {
+                    setState(() {
+                      _value = value!;
+                    });
+                  }),
+              const SizedBox(height: 8),
+              const Text("Subtraction")
+            ],
+          ),
+          Row(
+            children: [
+              Radio(
+                  value: 3,
+                  groupValue: _value,
+                  onChanged: (value) {
+                    setState(() {
+                      _value = value!;
+                    });
+                  }),
+              const SizedBox(height: 8),
+              const Text("Multiplication")
+            ],
+          ),
+          Row(
+            children: [
+              Radio(
+                  value: 4,
+                  groupValue: _value,
+                  onChanged: (value) {
+                    setState(() {
+                      _value = value!;
+                    });
+                  }),
+              const SizedBox(height: 8),
+              const Text("Subtraction")
+            ],
+          ),
         ],
       ),
     );

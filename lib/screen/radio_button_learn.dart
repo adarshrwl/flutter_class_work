777976e250c0118f087e2e4e8eb0dd5eaa5e4922
divashbh <divashbh@gmail.com>
2024-04-25 22:38:24 +0545

    Radio Buttons

--- lib/screen/radio_button_learn.dart
+++ lib/screen/radio_button_learn.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class RadioButtonTry extends StatefulWidget {
@@ -9,14 +8,78 @@ class RadioButtonTry extends StatefulWidget {
 }
 
 class _RadioButtonTryState extends State<RadioButtonTry> {
+  int value = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amber[100],
-        title: const Text("Radio Button Try",style: TextStyle(
-          color: Colors.black12,fontWeight: FontWeight.bold
-        ),),
+        title: const Text(
+          "Radio Button Try",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(border: OutlineInputBorder()),
+            keyboardType: TextInputType.number,
+            
+          ),
+          const SizedBox(
+            height: 9,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(border: OutlineInputBorder()),
+            keyboardType: TextInputType.number,
+          ),
+          Row(
+            children: [
+              Radio(
+                  value: 1,
+                  groupValue: 1,
+                  onChanged: (value) {
+                    value = value;
+                  }),
+              const Text("Number 1")
+            ],
+          ),
+          Row(
+            children: [
+              Radio(
+                  value: 2,
+                  groupValue: 1,
+                  onChanged: (value) {
+                    value = value;
+                  }),
+              const Text("Number 2")
+            ],
+          ),
+          Row(
+            children: [
+              Radio(
+                  value: 3,
+                  groupValue: 1,
+                  onChanged: (value) {
+                    value = value;
+                  }),
+              const Text("Number 3")
+            ],
+          ),
+          Row(
+            children: [
+              Radio(
+                  value: 4,
+                  groupValue: 1,
+                  onChanged: (value) {
+                    value = value;
+                  }),
+              const Text("Number 4")
+            ],
+          )
+        ],
       ),
     );
   }

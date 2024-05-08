5fb1af19b420ae7b603c3d2a495d699eb0a42d59
Adarsh Rawal <adarshrawal321@gmail.com>
2024-05-08 12:30:41 +0545

    added mediaquery

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,45 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
         appBar: AppBar(
           title: const Text("Profile Screen"),
           centerTitle: true,
           backgroundColor: Colors.blue[100],
         ),
-        body: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            Container(
-              height: 400,
-              width: double.infinity,
-              color: Colors.amber[100],
-              child: const Text("First Container"),
-            ),
-            Positioned(
-              top: 1,
-              right: 1,
-              child: Container(
-                height: 100,
-                width: 100,
-                color: Colors.green,
-                child: const Text("Second Container"),
+        body: GestureDetector(
+          onTap: () {
+            print("Hello");
+          },
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                height: screenHeight * .4,
+                width: screenWidth,
+                color: Colors.amber[100],
+                child: const Text("First Container"),
               ),
-            ),
-            Positioned(
-                bottom: -30,
-                left: 400 / 2 - 50,
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.blue[100],
-                  child: const Text("Bottom left"),
-                ))
-          ],
+              GestureDetector(
+                onDoubleTap: () {
+                  print("This is double tapped");
+                },
+                child: Positioned(
+                  top: 1,
+                  right: 1,
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.green,
+                    child: const Text("Second Container"),
+                  ),
+                ),
+              ),
+              Positioned(
+                  bottom: -30,
+                  left: 400 / 2 - 50,
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.blue[100],
+                    child: const Text("Bottom left"),
+                  ))
+            ],
+          ),
         ));
   }
 }
